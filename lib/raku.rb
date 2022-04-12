0e035d10d3e3f6f1adb035e730f7fbdec4bcc765
nonnax <ironald@gmail.com>
2022-04-12 14:51:59 +0800

    reformat

--- lib/raku.rb
+++ lib/raku.rb
@@ -2,33 +2,27 @@
 # Id$ nonnax 2022-04-12 11:22:22 +0800
 # raku : as cute as possible :> rack router
 require 'forwardable'
-
 D=Object.method(:define_method)
 module Raku  
   extend Forwardable
   def_delegators :stack, :map, :use
-
-  maps ||= Hash[]
+  maps=Hash[]
   D.(:mapping){ maps }
   %w(GET POST PUT DELETE).map do |m| D.(m.downcase){ |*u, &block| u.flatten.each{|x| maps[[m, x]]=block } } end
   def self.call(env) @app.call(env)  end
-  def self.new
-    stack.run App.new
-    stack
-  end
-  def self.stack
-    @stack || =Rack::Builder.new do
-      use Rack::Static, urls: %w[/images /js /css], root: 'public'      
-    end
-  end
+  def self.new() stack.run App.new; stack end
+  def self.stack() @stack ||= Rack::Builder.new{ use Rack::Static, urls: %w[/images /js /css], root: 'public' } end
   class App
     attr :res, :req, :env
-    def call(env)
+    def _call(env)
       catch(:halt) {
         @req, @res, @nv=Rack::Request.new(env), Rack::Response.new, env
         res.write(body=instance_exec(req.params, &::Raku.mapping[env.values_at('REQUEST_METHOD', 'PATH_INFO')]) rescue nil)
         body ? res.finish : not_found
       }
+    end
+    def call(env)
+      dup._call(env)
     end
     def not_found() [404, {}, ['Not Found']] end
     def halt(res) throw :halt, res end      # bypass
